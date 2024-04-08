f117bd7aaf56cd4d6f162aa8458f3af24a582eed
tomason182 <elotro182@hotmail.com>
2024-04-08 15:43:16 -0300

    add rules configs for style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,14 @@ module.exports = {
             template: './src/index.html',
         }),
     ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
