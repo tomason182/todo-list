2481b4027c29e2fef25e184d1a5e2364efdaba84
tomason182 <elotro182@hotmail.com>
2024-04-08 19:17:11 -0300

    create handleSidebarClicks function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles.css';
+import { showMyDay } from './todo';
+
+handleSidebarClicks = ()=> {
+    const sidebarItems = document.querySelector('.content > .sidebar-container > .sidebar-items');
+
+    sidebarItems.addEventListener('click', () => {
+        showMyDay();
+    });
+}
+
 
 console.log('everything is working awesome!')
