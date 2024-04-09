8f9c9becdf4dfee4e8afdf29902cf4f2e83cd0ec
tomason182 <elotro182@hotmail.com>
2024-04-09 10:21:03 -0300

    add header to myDay container and create a dates function for testing

--- src/todo.js
+++ src/todo.js
@@ -7,13 +7,29 @@ function showMyDay(){
     const dayContainer = document.createElement('div');
     dayContainer.classList.add('day-container');
     
-    const dayTitle = document.createElement('h2');
-    dayTitle.textContent = 'My Day';
-    
-    dayContainer.appendChild(dayTitle);
+    const dayHeader = document.createElement('div');
+    dayHeader.classList.add('day-header');
 
+    const dayTitle = document.createElement('h2');
+    dayTitle.classList.add('day-title');
+    dayTitle.textContent = 'My Day';
+
+    const todayDate = document.createElement('h2');
+    todayDate.classList.add('today-date');
+    todayDate.textContent = dates(); //should call a class that return formatted date
+
+    dayHeader.appendChild(dayTitle);
+    dayHeader.appendChild(todayDate);    
+    dayContainer.appendChild(dayHeader);
     mainContainer.appendChild(dayContainer);
 
+}
+
+function dates() {
+    const today = new Date();
+    const day = today.getDate();
+    const month = today.getMonth();
+    return `${day}.${month}`
 }
 
 export {showMyDay}
