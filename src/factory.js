a91c511773a4ac1e593dec769d4b5faf14587bac
tomason182 <elotro182@hotmail.com>
2024-04-10 13:40:34 -0300

    add storageAvailable function
    this function test if localStorage is abailable in the browser caching differents kind of errors

--- src/factory.js
+++ src/factory.js
@@ -34,3 +34,34 @@ function createTask (title, description, dueDate = new Date(), priority, status)
 
     return { ...task, editTaskTitle, editDueDate, editPriority, editStatus};
 }
+
+function storeTaskInLocalStorage (newTask){
+    localStorage.setItem()
+}
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x,x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            //all browsers except firefox
+            (e.code === 22 ||
+            //firefox
+            e.code === 1014 ||
+            //test name field too, because code might not be present
+            //all browsers except firefox
+            e.name === "QuotaExceededError" ||
+            //firefox
+            e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+            //acknowledge QuotaExceedError only if there is something already store
+            storage &&
+            storage.length !== 0
+        );
+    }
+}
