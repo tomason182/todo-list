d18c48d4b416ce5a6df0b045f3efe5b8679a27c0
tomason182 <elotro182@hotmail.com>
2024-04-15 19:11:21 -0300

    add handleProject function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import './styles.css';
 import { showMyDay } from './todo';
 
-const handleSidebarClicks = ()=> {
+const handleSidebarClicks = () => {
     const sidebarItems = document.querySelector('.content > .sidebar-container > .sidebar-items');
 
     sidebarItems.addEventListener('click', () => {
         showMyDay();
     });
+    
+    handleProjects();
+}
+
+const handleProjects = () => {
+    const addProject = document.querySelector('.project-container-header > .add-project-button');
+
+    addProject.addEventListener('click', () => {
+        console.log('the add project function has been display')
+    })
 }
 
 handleSidebarClicks();
 
+
 showMyDay();
 
-console.log('everything is working awesome!')
+console.log('everything is working awesome!');
