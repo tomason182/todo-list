73635907b054e2825c068f28eb7eafe162c53b61
tomason182 <elotro182@hotmail.com>
2024-04-15 19:32:41 -0300

    add listener for project inside handleSidebarClicks
    delete the previous function handleProject function and add the eventListener inside the handleSidebarClicks function directly

--- src/index.js
+++ src/index.js
@@ -3,20 +3,16 @@ import { showMyDay } from './todo';
 
 const handleSidebarClicks = () => {
     const sidebarItems = document.querySelector('.content > .sidebar-container > .sidebar-items');
+    const addProject = document.querySelector('.project-container-header > .add-project-button');
 
     sidebarItems.addEventListener('click', () => {
         showMyDay();
     });
-    
-    handleProjects();
-}
-
-const handleProjects = () => {
-    const addProject = document.querySelector('.project-container-header > .add-project-button');
 
     addProject.addEventListener('click', () => {
-        console.log('the add project function has been display')
+        console.log('adding project still working');
     })
+    
 }
 
 handleSidebarClicks();
