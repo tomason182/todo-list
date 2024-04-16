ff2943900cc5e9e5229ff4aed15f115bf587bea9
tomason182 <elotro182@hotmail.com>
2024-04-15 23:29:45 -0300

    show dialog when click

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import { showMyDay } from './todo';
 const handleSidebarClicks = () => {
     const sidebarItems = document.querySelector('.content > .sidebar-container > .sidebar-items');
     const addProject = document.querySelector('.project-container-header > .add-project-button');
+    const projectDialog = document.getElementById('project-dialog')
 
     sidebarItems.addEventListener('click', () => {
         showMyDay();
     });
 
     addProject.addEventListener('click', () => {
-        console.log('adding project still working');
+        projectDialog.showModal();
     });
 }
 
