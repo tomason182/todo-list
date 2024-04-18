f30c754453209da928d387b91150a05cc59715b8
tomason182 <elotro182@hotmail.com>
2024-04-18 18:52:19 -0300

    fix indentation

--- src/index.js
+++ src/index.js
@@ -2,61 +2,61 @@ import './styles.css';
 import { showMyDay, handleProjectsInputs } from './todo';
 
 const handleSidebarElements = () => {
-    const sidebarItems = document.querySelector('.content > .sidebar-container > .sidebar-items');
-    const addProject = document.querySelector('.project-container-header > .add-project-button');
-    const projectDialog = document.getElementById('project-dialog');
-    const cancelProjectDialog = document.getElementById('cancel-add-project-dialog');
-    const submitProjectButton = document.getElementById('project-submit');
-    const projectInput = document.getElementById('project-input');
+  const sidebarItems = document.querySelector('.content > .sidebar-container > .sidebar-items');
+  const addProject = document.querySelector('.project-container-header > .add-project-button');
+  const projectDialog = document.getElementById('project-dialog');
+  const cancelProjectDialog = document.getElementById('cancel-add-project-dialog');
+  const submitProjectButton = document.getElementById('project-submit');
+  const projectInput = document.getElementById('project-input');
 
-    sidebarItems.addEventListener('click', (event) => {
-        const clickedItem = event.target;
+  sidebarItems.addEventListener('click', (event) => {
+    const clickedItem = event.target;
 
-        if (clickedItem.classList[0] === 'my-day'){
-            console.log('my day was pressed');
-        }else if (clickedItem.classList[0] === 'week'){
-            console.log('the week element was pressed');
-        }else if (clickedItem.classList[0] === 'calendar'){
-            console.log('the calendar was pressed');
-        }
-    });
+    if (clickedItem.classList[0] === 'my-day'){
+      console.log('my day was pressed');
+    }else if (clickedItem.classList[0] === 'week'){
+      console.log('the week element was pressed');
+    }else if (clickedItem.classList[0] === 'calendar'){
+      console.log('the calendar was pressed');
+    }
+  });
 
-    addProject.addEventListener('click', () => {
-        projectDialog.showModal();
-        handleProjectSelection();
-    });
-    
-    cancelProjectDialog.addEventListener('click', () => {
-        projectDialog.close();
-    });
-
-    //handle new projects inputs 
-
-    submitProjectButton.addEventListener('click', (e) => {
-        handleProjectsInputs(e);
-        projectDialog.close();
-        }); 
-
-    projectInput.addEventListener('keydown', (e) => {        
-        if(e.key === 'Enter'){
-            handleProjectsInputs(e);
-            projectDialog.close();
-        }
-    });
-
+  addProject.addEventListener('click', () => {
+    projectDialog.showModal();
     handleProjectSelection();
+  });
+    
+  cancelProjectDialog.addEventListener('click', () => {
+    projectDialog.close();
+  });
+
+  //handle new projects inputs 
+
+  submitProjectButton.addEventListener('click', (e) => {
+    handleProjectsInputs(e);
+    projectDialog.close();
+  }); 
+
+  projectInput.addEventListener('keydown', (e) => {        
+    if(e.key === 'Enter'){
+      handleProjectsInputs(e);
+      projectDialog.close();
+    }
+  });
+
+  handleProjectSelection();
 }
 
 const handleProjectSelection = () => {
-    const projectSelection = document.querySelector('.project-list');
+  const projectSelection = document.querySelector('.project-list');
 
-    if(projectSelection !== null) {
-        projectSelection.addEventListener('click', (event) => {
-            const clickedProject = event.target;
+  if(projectSelection !== null) {
+    projectSelection.addEventListener('click', (event) => {
+      const clickedProject = event.target;
     
-            console.log(clickedProject);
-        })
-    }
+      console.log(clickedProject);
+    })
+  }
 }
 
 handleSidebarElements();
